e4d5ab03aaca5735bdfba8d4a245cbd1345186d3
Youssoufou Mohamed <mohamed.youssoufou@smile.ci>
2021-08-19 09:31:25 +0000

    ...

--- components/qui-somme-nous.vue
+++ components/qui-somme-nous.vue
@@ -4,16 +4,16 @@
       <h2 class="col-md-12 title">Qui sommes nous?</h2>
       <div class="col-md-12">
         <p class="btn-body-text">
-          Wealthtequich innovation est une start-up spécialisée dans le
+          Wealthtech innovations est une start-up spécialisée dans le
         développement technique d’applications blockchain et dans la promotion
-        d’une nouvelle économie numérique. Nous avons pour objectif est d’être
+        d’une nouvelle économie numérique.<br/> Nous avons pour objectif d’être
         un des leaders africains de l'accompagnement stratégique et technique
-        sur les technologies blockchain et du conseil sur les actifs numérique
-        (Crypto-actifs). Suite au constat d’absence d’informations et de
-        formation sur le sujet de la blockchain en Afrique, nous avons décidé de
+        sur les technologies blockchain et dans le conseil sur les actifs numériques
+        (Crypto-actifs).<br/> Suite au constat d’absence d’informations et de
+        formations sur le sujet de la blockchain en Afrique, nous avons décidé de
         mettre en place une structure pour promouvoir cette nouvelle technologie
-        et les avantages liés à son implémentation. Nous avons notamment décidé
-        de mettre en place des partenariats avec des universités et des
+        et les avantages liés à son implémentation. <br/>Nous avons notamment mis
+        en place des partenariats avec des universités et des
         associations dans l'optique d'informer, de former et de concevoir des
         projets en ce sens.
         </p>
